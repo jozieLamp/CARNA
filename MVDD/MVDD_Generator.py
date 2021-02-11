8561fea287a5bcec6263f9a7756907e31b40a074
Josie Lamp <jl4rj@virginia.edu>
2021-02-11 11:02:39 -0500

    Adding training withnew clusters

--- MVDD/MVDD_Generator.py
+++ MVDD/MVDD_Generator.py
@@ -193,7 +193,7 @@ def getBestMVDD(dt, xData, yData, classes, learningCriteria):
     edgeList = []
 
     # get all combos of edges
-    edgeOptions = genEdgeCombos(percentReqdMin, percentReqdMax, totalEdges, combinationSize=10, sampleSize=10) #combinationSize=100000, sampleSize=50000) #changing stuff here
+    edgeOptions = genEdgeCombos(percentReqdMin, percentReqdMax, totalEdges, combinationSize=1000, sampleSize=500) #combinationSize=100000, sampleSize=50000) #changing stuff here
 
     #Exhaustive sample of MVDD edges to try, and get best resulting MVDD
     for edgeOpt in edgeOptions:
