723eee39cfe9cf211a3f2e9787acacdd6edbaeb3
jozieLamp <jl4rj@virginia.edu>
2021-04-27 15:09:38 -0400

    Fixing clustering and training datasets

--- MVDD/MVDD_Generator.py
+++ MVDD/MVDD_Generator.py
@@ -245,7 +245,7 @@ def getBestMVDD(dt, xData, yData, classes, learningCriteria):
     edgeList = []
 
     # get all combos of edges
-    edgeOptions = genEdgeCombos(percentReqdMin, percentReqdMax, totalEdges, combinationSize=5, sampleSize=5) #combinationSize=100000, sampleSize=50000) #changing stuff here
+    edgeOptions = genEdgeCombos(percentReqdMin, percentReqdMax, totalEdges, combinationSize=300, sampleSize=150) #combinationSize=100000, sampleSize=50000) #changing stuff here
 
     #Exhaustive sample of MVDD edges to try, and get best resulting MVDD
     for edgeOpt in edgeOptions:
